55dd081a39415b01a494eff9d304883ab66ad459
Hirokazu NISHIOKA <hiro@nisshiee.org>
2012-06-10 22:59:48 +0900

    versionを1.1.0-SNAPSHOTにセット

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "to-ban"
-    val appVersion      = "1.0.0"
+    val appVersion      = "1.1.0-SNAPSHOT"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
